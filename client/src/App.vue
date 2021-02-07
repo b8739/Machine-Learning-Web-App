effe47418202e082d14958844957f061fcf3decb
b8739 <b8739@hanmail.net>
2021-02-07 22:53:45 +0900

    vue에 데이터 업로드하면 flask에서 받도록 설정 완료

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    Jo
+    <div class="container">
+      <!-- file Uploader -->
+      <div class="fileUploader">
+        <div class = "uploadGuide">
+          <h2 style="color:#fff">Drag Your File Here</h2>
+          <p style="color:#fff">OR</p>
+        </div>
+        <form method=POST enctype=multipart/form-data action="http://localhost:5000/dataupload" class="">
+          <input type="file" name="csv_data">
+          <input type="submit"  class="">
+          <input type="reset"  value="Reset" class="">
+        </form>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<!-- style -->
+<style>
+#app {
+  margin-top: 60px
+}
+</style>
